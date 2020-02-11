e29a369cb6ad3f173ffb6d3481cb1271013b5b8f
Fred Wills <willsf422@gmail.com>
2020-02-10 23:37:10 -0500

    account list works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccountListComponent } from './feature/account-list/account-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: AccountListComponent},
+
+  {path: '**', component: AccountListComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
